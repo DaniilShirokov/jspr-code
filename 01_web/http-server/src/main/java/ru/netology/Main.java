b252a802aeff3a34bb0e528bfa67f5221db44d3a
Damiil Shirokov <daniil196196@gmail.com>
2024-08-24 22:46:24 +0500

    change test

--- 01_web/http-server/src/main/java/ru/netology/Main.java
+++ 01_web/http-server/src/main/java/ru/netology/Main.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class Main {
   public static void main(String[] args) {
     final var validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
-
+    System.out.println("test");
     try (final var serverSocket = new ServerSocket(9999)) {
       while (true) {
         try (
